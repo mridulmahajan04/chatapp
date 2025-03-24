497c6eaedc9b663ece8da058b165abf4bb8940d4
mridulmahajan04 <mridulmahajan16@gmail.com>
2025-03-24 10:21:18 +0530

    First Commit

---
+++ Frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import SearchInput from "./SearchInput";
+
+const Sidebar = () => {
+	return (
+		<div className='border-r border-slate-500 p-4 flex flex-col'>
+			<SearchInput />
+			<div className='divider px-3'></div>
+		</div>
+	);
+};
+export default Sidebar;
